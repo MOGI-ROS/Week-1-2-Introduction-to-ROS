82b4bb91ed5f54eab01618fc0469d0dcd3336c2d
david.dudas <david.dudas@outlook.com>
2021-01-04 12:31:11 +0100

    fix custom publisher comments

--- bme_ros_tutorials/scripts/publisher_custom.py
+++ bme_ros_tutorials/scripts/publisher_custom.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 
 import rospy
-from bme_ros_tutorials.msg import Test  # Message type used in the node
+from bme_ros_tutorials.msg import Test # Our custom message type used in the node
 
-rospy.init_node('publisher_custom')    # Init the node with name "publisher"
+rospy.init_node('publisher_custom')    # Init the node with name "publisher_custom"
 
 pub = rospy.Publisher('publisher_custom_topic', Test, queue_size=10)
 
-rospy.loginfo("Publisher Python node has started and publishing data on publisher_topic")
+rospy.loginfo("Custom publisher Python node has started and publishing data on publisher_custom_topic")
 
-rate = rospy.Rate(1)            # 1Hz
+rate = rospy.Rate(5) # 5Hz
 
 testMessage = Test()
 testMessage.first_name = "David"
 testMessage.last_name  = "Dudas"
-testMessage.age        = 8
+testMessage.age        = 32
 testMessage.score      = 0
 
-while not rospy.is_shutdown():  # Run the node until Ctrl-C is pressed
+while not rospy.is_shutdown(): # Run the node until Ctrl-C is pressed
 
-	pub.publish(testMessage)          # Publishing data on topic "publisher_topic"
-  
-	testMessage.score += 1
-	
-	rate.sleep()                # The loop runs at 1Hz
+    pub.publish(testMessage)   # Publishing data on topic "publisher_custom_topic"
+    
+    testMessage.score += 1
+    
+    rate.sleep()               # The loop runs at 5Hz
--- bme_ros_tutorials/src/publisher_custom.cpp
+++ bme_ros_tutorials/src/publisher_custom.cpp
@@ -1,45 +1,27 @@
 #include "ros/ros.h"
-#include "bme_ros_tutorials/Test.h"             // Message type used in the node
+#include "bme_ros_tutorials/Test.h" // Our custom message type used in the node
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "publisher_custom"); // Init the node with name "publisher"
-	ros::NodeHandle nh;                 // NodeHandle will fully initialze the node
-
-    /**
-    * The advertise() function is how you tell ROS that you want to
-    * publish on a given topic name. This invokes a call to the ROS
-    * master node, which keeps a registry of who is publishing and who
-    * is subscribing. After this advertise() call is made, the master
-    * node will notify anyone who is trying to subscribe to this topic name,
-    * and they will in turn negotiate a peer-to-peer connection with this
-    * node.  advertise() returns a Publisher object which allows you to
-    * publish messages on that topic through a call to publish().  Once
-    * all copies of the returned Publisher object are destroyed, the topic
-    * will be automatically unadvertised.
-    *
-    * The second parameter to advertise() is the size of the message queue
-    * used for publishing messages.  If messages are published more quickly
-    * than we can send them, the number here specifies how many messages to
-    * buffer up before throwing some away.
-    */
+	ros::init(argc, argv, "publisher_custom"); // Init the node with name "publisher_custom"
+	ros::NodeHandle nh;                        // NodeHandle will fully initialze the node
 
 	ros::Publisher pub = nh.advertise<bme_ros_tutorials::Test>("publisher_custom_topic", 10);
 	
-	ROS_INFO("Publisher C++ node has started and publishing data on publisher_custom_topic");
+	ROS_INFO("Custom publisher C++ node has started and publishing data on publisher_custom_topic");
 	
-	ros::Rate loop_rate(1); // 1 Hz
+	ros::Rate loop_rate(5); // 5 Hz
 	
-	bme_ros_tutorials::Test test_message;  // Count is now a ROS Int32 type variable that is ready to be published
-	test_message.first_name="David";           // Initializing count
-    test_message.last_name="Dudas";           // Initializing count
-    test_message.age=32;           // Initializing count
-    test_message.score=0;           // Initializing count
+	bme_ros_tutorials::Test test_message; // test_message is now our custom ROS Test type variable that is ready to be published
+	test_message.first_name = "David";    // Initializing first name string
+    test_message.last_name  = "Dudas";    // Initializing last name string
+    test_message.age        = 32;         // Initializing age
+    test_message.score      = 0;          // Initializing score that we'll increment later
 	
-    while (ros::ok())       // Run the node until Ctrl-C is pressed
+    while (ros::ok()) // Run the node until Ctrl-C is pressed
     {		
-		pub.publish(test_message); // Publishing data on topic "publisher_topic"
+		pub.publish(test_message); // Publishing data on topic "publisher_custom_topic"
 		test_message.score++;	
-		loop_rate.sleep();  // The loop runs at 1Hz
+		loop_rate.sleep();         // The loop runs at 5Hz
 	}
 }
